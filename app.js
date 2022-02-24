2da0d12e943a1dce47f10d4fc59801329bc369d0
mukulsinghal407 <79706982+mukulsinghal407@users.noreply.github.com>
2022-02-24 18:31:17 +0530

    port

--- app.js
+++ app.js
@@ -186,6 +186,6 @@ app.post("/register",function(req,res){
 //     }); 
 // });
 
-app.listen(process.env.port||5000,(req,res)=>{
+app.listen(process.env.PORT||5000,(req,res)=>{
     console.log("Server Started at 3000");
 });
